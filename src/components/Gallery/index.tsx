731706d933af840ee6fbd3faa44ea3070610b6dc
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2023-11-11 23:12:31 -0300

    integracao com api

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,119 @@
+import { useState } from 'react'
+import spiderMan from '../../assets/images/banner-homem-aranha.png'
+import play from '../../assets/images/play.png'
+import zoom from '../../assets/images/zoom.png'
+import fechar from '../../assets/images/fechar.png'
+
+import Section from '../../components/Section'
+import { Items, Item, Action, Modal, ModalContent } from './styles'
+import { GalleryItem } from '../../pages/Home'
+
+const mock: GalleryItem[] = [
+  {
+    type: 'image',
+    url: spiderMan
+  },
+  {
+    type: 'image',
+    url: spiderMan
+  },
+  {
+    type: 'video',
+    url: 'https://www.youtube.com/embed/78CP8na1Fpo?si=P8Ad3teABGdtljCa'
+  }
+]
+
+type Props = {
+  defaultCover: string
+  name: string
+  items: GalleryItem[]
+}
+
+interface ModalState extends GalleryItem {
+  isVisible: boolean
+}
+
+const Gallery = ({ defaultCover, name, items }: Props) => {
+  const [modal, setModal] = useState<ModalState>({
+    isVisible: false,
+    type: 'image',
+    url: ''
+  })
+
+  const getMidiaCover = (item: GalleryItem) => {
+    if (item.type === 'image') return item.url
+    return defaultCover
+  }
+
+  const getMidiaIcon = (item: GalleryItem) => {
+    if (item.type === 'image') return zoom
+    return play
+  }
+
+  const closeModal = () => {
+    setModal({
+      isVisible: false,
+      type: 'image',
+      url: ''
+    })
+  }
+
+  return (
+    <>
+      <Section title="Galeria" background="black">
+        <Items>
+          {items.map((media, index) => (
+            <Item
+              key={media.url}
+              onClick={() => {
+                setModal({
+                  isVisible: true,
+                  type: media.type,
+                  url: media.url
+                })
+              }}
+            >
+              <img
+                src={getMidiaCover(media)}
+                alt={`midia ${index + 1} de ${name}`}
+              />
+              <Action>
+                <img
+                  src={getMidiaIcon(media)}
+                  alt="clique para maximinizar a mídia"
+                />
+              </Action>
+            </Item>
+          ))}
+        </Items>
+      </Section>
+      <Modal className={modal.isVisible ? 'visivel' : ''}>
+        <ModalContent className="container">
+          <header>
+            <h4>{name}</h4>
+            <img
+              src={fechar}
+              alt="clique aqui para fechar"
+              onClick={() => {
+                closeModal()
+              }}
+            />
+          </header>
+          {modal.type === 'image' ? (
+            <img src={modal.url} />
+          ) : (
+            <iframe frameBorder={0} src={modal.url} />
+          )}
+        </ModalContent>
+        <div
+          className="overlay"
+          onClick={() => {
+            closeModal()
+          }}
+        ></div>
+      </Modal>
+    </>
+  )
+}
+
+export default Gallery
--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,9 +1,53 @@
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import Hero from '../../components/Hero'
+import Section from '../../components/Section'
+import Gallery from '../../components/Gallery'
+import { Game } from '../Home'
 
 const Product = () => {
   const { id } = useParams()
 
-  return <div>produto{id}</div>
+  const [game, setGame] = useState<Game>()
+
+  useEffect(() => {
+    fetch(`https://fake-api-tau.vercel.app/api/eplay/jogos/${id}`)
+      .then((res) => res.json())
+      .then((res) => setGame(res))
+  }, [id])
+
+  if (!game) {
+    return <h3>Carregando...</h3>
+  }
+
+  return (
+    <>
+      <Hero game={game} />
+      <Section title="Sobre o jogo" background="black">
+        <p>{game.description}</p>
+      </Section>
+      <Section title="Mais detalhes" background="gray">
+        <p>
+          <b>Plataforma:</b> {game.details.system}
+          <br />
+          <b>Desenvolvedor:</b> {game.details.developer}
+          <br />
+          <b>Editora:</b> {game.details.publisher}
+          <br />
+          <b>Idiomas:</b> O jogo oferece suporte a diversos idiomas, incluindo{' '}
+          {game.details.languages.join(', ')}
+          <br />
+          As opções de áudio e legendas podem ser ajustadas nas configurações do
+          jogo.
+        </p>
+      </Section>
+      <Gallery
+        items={game.media.gallery}
+        defaultCover={game.media.cover}
+        name={game.name}
+      />
+    </>
+  )
 }
 
 export default Product
